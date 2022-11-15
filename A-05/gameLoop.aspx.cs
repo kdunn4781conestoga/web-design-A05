c39b786c0c181a35b50c6a0008123736655635de
kdunn4781 <101570040+kdunn4781conestoga@users.noreply.github.com>
2022-11-15 17:39:57 -0500

    Moved everything outside Init

--- A-05/gameLoop.aspx.cs
+++ A-05/gameLoop.aspx.cs
@@ -10,30 +10,6 @@ namespace A_05
 {
     public partial class gameLoop : System.Web.UI.Page
     {
-        protected void Page_Init(object sender, EventArgs e)
-        {
-            if (Session["playerName"] == null || string.IsNullOrEmpty(Session["playerName"].ToString()))
-            {
-                Response.Redirect("default.aspx");
-            }
-
-            int maximumNumber = 0;
-            if (Session["maximumNumber"] == null || !Int32.TryParse(Session["maximumNumber"].ToString(), out maximumNumber))
-            {
-                Response.Redirect("maximumNumber.aspx");
-            }
-
-            var rand = new Random();
-            if (Session["numberToGuess"] == null)
-            {
-                Session["numberToGuess"] = rand.Next(2, maximumNumber + 1);
-            }
-            Session["playerGuess"] = null;
-
-            userGuessRangeValidator.MinimumValue = 2.ToString();
-            userGuessRangeValidator.MaximumValue = maximumNumber.ToString();
-        }
-
         protected void Page_Load(object sender, EventArgs e)
         {
             ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
@@ -41,12 +17,35 @@ namespace A_05
             string playerName = Session["playerName"] as string;
             int? numberToGuess = Session["numberToGuess"] as int?;
 
-            if (IsPostBack)
+            if (!IsPostBack)
+            {
+                if (Session["playerName"] == null || string.IsNullOrEmpty(Session["playerName"].ToString()))
+                {
+                    Response.Redirect("default.aspx");
+                }
+
+                int maximumNumber = 0;
+                if (Session["maximumNumber"] == null || !Int32.TryParse(Session["maximumNumber"].ToString(), out maximumNumber))
+                {
+                    Response.Redirect("maximumNumber.aspx");
+                }
+
+                var rand = new Random();
+                if (numberToGuess == null)
+                {
+                    numberToGuess = rand.Next(2, maximumNumber + 1);
+
+                    Session["numberToGuess"] = numberToGuess;
+                }
+                Session["playerGuess"] = null;
+
+                userGuessRangeValidator.MinimumValue = 2.ToString();
+                userGuessRangeValidator.MaximumValue = maximumNumber.ToString();
+            }
+            else
             {
 
             }
-
-
 
             guessLbl.Text = $"<b>{playerName}</b><br /> <b>{numberToGuess}</b>Please enter your guess.";
         }
